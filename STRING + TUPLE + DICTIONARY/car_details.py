bf3f9d759ccd4f2a889dcb2f47b0480bf374cfa6
sohitkumarmca2025-cmd <sohit.kumar.mca.2025@miet.ac.in>
2026-02-23 23:02:26 +0530

    Create car_details.py

---
+++ STRING + TUPLE + DICTIONARY/car_details.py
@@ -0,0 +1,13 @@
+# File: car_details.py
+
+car = {
+    "brand": "Toyota",
+    "specs": ("Petrol", "Manual", "Red")
+}
+
+print("Car Brand:", car["brand"])
+print("Specifications:", car["specs"])
+
+# Output:
+# Car Brand: Toyota
+# Specifications: ('Petrol', 'Manual', 'Red')
