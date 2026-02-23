08037298c0d520cf9791a1d9b69af7d3aee1dbca
sohitkumarmca2025-cmd <sohit.kumar.mca.2025@miet.ac.in>
2026-02-23 23:09:27 +0530

    Create list_deletion_methods.py

---
+++ 23_feb_class/list_deletion_methods.py
@@ -0,0 +1,17 @@
+# File: list_deletion_methods.py
+
+data = [10, 20, 30, 40, 50]
+
+# remove()
+data.remove(20)
+
+# pop()
+data.pop()
+
+# del
+del data[1]
+
+print("After Deletions:", data)
+
+# Output:
+# After Deletions: [10, 40]
