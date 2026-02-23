a206e2fa03ac678127c56be246b90779ff0d7d8a
sohitkumarmca2025-cmd <sohit.kumar.mca.2025@miet.ac.in>
2026-02-23 14:02:39 +0530

    Create E_Commerce_Cart_System.py

---
+++ list_in_python_lab_1/E_Commerce_Cart_System.py
@@ -0,0 +1,30 @@
+# Program Name: E-Commerce Cart System (Advanced)
+
+cart = [2000, 1500, 2000, 800, 1200]
+
+# Remove duplicates
+cart = list(set(cart))
+
+subtotal = sum(cart)
+
+discount = 0
+if subtotal > 5000:
+    discount = subtotal * 0.10
+
+after_discount = subtotal - discount
+
+gst = after_discount * 0.18
+final_amount = after_discount + gst
+
+print("Unique Items:", cart)
+print("Subtotal:", subtotal)
+print("Discount:", discount)
+print("GST (18%):", round(gst,2))
+print("Final Payable Amount:", round(final_amount,2))
+
+# Output:
+# Unique Items: [2000, 1500, 800, 1200]
+# Subtotal: 5500
+# Discount: 550.0
+# GST (18%): 891.0
+# Final Payable Amount: 5841.0
