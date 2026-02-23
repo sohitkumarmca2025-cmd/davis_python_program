659aab78e1090a7477386b9316c02e17ebc4b32d
sohitkumarmca2025-cmd <sohit.kumar.mca.2025@miet.ac.in>
2026-02-23 23:07:03 +0530

    Create List_Functions.py

---
+++ 23_feb_class/List_Functions.py
@@ -0,0 +1,13 @@
+# File: List_Functions.py
+
+numbers = [10, 20, 30, 40]
+
+numbers.append(50)
+numbers.insert(1, 15)
+numbers.remove(30)
+numbers.sort()
+
+print("Final List:", numbers)
+
+# Output:
+# Final List: [10, 15, 20, 40, 50]
