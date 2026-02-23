037d2f1945fe2bb0d3ff53a9668f7bf5893e8efb
sohitkumarmca2025-cmd <sohit.kumar.mca.2025@miet.ac.in>
2026-02-23 23:04:27 +0530

    Create fruit_store.py

---
+++ STRING + TUPLE + DICTIONARY/fruit_store.py
@@ -0,0 +1,13 @@
+# File: fruit_store.py
+
+store = {
+    "fruits": ("Apple", "Banana", "Mango"),
+    "owner": "Suresh"
+}
+
+print("Owner:", store["owner"])
+print("Available Fruits:", store["fruits"])
+
+# Output:
+# Owner: Suresh
+# Available Fruits: ('Apple', 'Banana', 'Mango')
