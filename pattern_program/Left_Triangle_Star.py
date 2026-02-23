b30094f64aed005c8aa0536c4b63bcd6a86c127e
sohitkumarmca2025-cmd <sohit.kumar.mca.2025@miet.ac.in>
2026-02-23 12:11:24 +0530

    Create Left_Triangle_Star.py

---
+++ pattern_program/Left_Triangle_Star.py
@@ -0,0 +1,13 @@
+# Program Name: Left Triangle Star Pattern
+
+rows = int(input("Enter number of rows: "))
+
+for i in range(1, rows + 1):
+    print("*" * i)
+
+# Output:
+# Enter number of rows: 4
+# *
+# **
+# ***
+# ****
