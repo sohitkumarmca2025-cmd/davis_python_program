b03fdebaebf69569229e0573f9638b6908207355
sohitkumarmca2025-cmd <sohit.kumar.mca.2025@miet.ac.in>
2026-02-23 15:08:29 +0530

    Create triangle_verification.py

---
+++ conditional_statement/triangle_verification.py
@@ -0,0 +1,24 @@
+# Program Name: Triangle Verification
+
+a = float(input("Enter side 1: "))
+b = float(input("Enter side 2: "))
+c = float(input("Enter side 3: "))
+
+if a + b > c and a + c > b and b + c > a:
+    print("Valid Triangle")
+    
+    if a == b == c:
+        print("Equilateral Triangle")
+    elif a == b or b == c or a == c:
+        print("Isosceles Triangle")
+    else:
+        print("Scalene Triangle")
+else:
+    print("Not a Valid Triangle")
+
+# Output:
+# Enter side 1: 5
+# Enter side 2: 5
+# Enter side 3: 5
+# Valid Triangle
+# Equilateral Triangle
