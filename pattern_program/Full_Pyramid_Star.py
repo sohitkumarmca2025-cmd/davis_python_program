448621b091e61585c0e1c0b5c24e26eafd969a4a
sohitkumarmca2025-cmd <sohit.kumar.mca.2025@miet.ac.in>
2026-02-23 12:15:14 +0530

    Create Full_Pyramid_Star.py

---
+++ pattern_program/Full_Pyramid_Star.py
@@ -0,0 +1,11 @@
+# Program Name: Full Pyramid Star Pattern
+
+rows = int(input("Enter number of rows: "))
+
+for i in range(1, rows + 1):
+    print(" " * (rows - i) + "*" * (2*i - 1))
+
+# Output:
+#   *
+#  ***
+# *****
