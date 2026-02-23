135e86f91541deb2ada5e9a40388bca5d2494712
sohitkumarmca2025-cmd <sohit.kumar.mca.2025@miet.ac.in>
2026-02-23 12:14:28 +0530

    Create Inverted_Right_Triangle.py

---
+++ pattern_program/Inverted_Right_Triangle.py
@@ -0,0 +1,12 @@
+# Program Name: Inverted Right Triangle
+
+rows = int(input("Enter number of rows: "))
+
+for i in range(rows, 0, -1):
+    print(" " * (rows - i) + "*" * i)
+
+# Output:
+# ****
+#  ***
+#   **
+#    *
