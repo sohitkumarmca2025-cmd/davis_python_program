b2424f444e3417c372fd1cf9be2e05d181ba869f
sohitkumarmca2025-cmd <sohit.kumar.mca.2025@miet.ac.in>
2026-02-23 12:21:01 +0530

    Create Hollow_Square_Advanced.py

---
+++ pattern_program/Hollow_Square_Advanced.py
@@ -0,0 +1,18 @@
+# Program Name: Hollow Square Advanced Pattern
+
+n = int(input("Enter size: "))
+
+for i in range(n):
+    for j in range(n):
+        if i == 0 or i == n-1 or j == 0 or j == n-1:
+            print("* ", end="")
+        else:
+            print("  ", end="")
+    print()
+
+# Output:
+# Enter size: 4
+# * * * *
+# *     *
+# *     *
+# * * * *
