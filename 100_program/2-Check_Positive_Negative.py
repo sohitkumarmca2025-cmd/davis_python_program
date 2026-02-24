a9ddb71a63f9a0cf8871005e4440c2e861c89646
sohitkumarmca2025-cmd <sohit.kumar.mca.2025@miet.ac.in>
2026-02-24 10:22:36 +0530

    Create 2-Check_Positive_Negative.py

---
+++ 100_program/2-Check_Positive_Negative.py
@@ -0,0 +1,14 @@
+# Check number type
+
+num = int(input("Enter a number: "))
+
+if num > 0:
+    print("Positive Number")
+elif num < 0:
+    print("Negative Number")
+else:
+    print("Zero")
+
+# Output:
+# Enter a number: -5
+# Negative Number
