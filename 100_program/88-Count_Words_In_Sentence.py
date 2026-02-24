332b3b343c92af93c1d881c543c4d40125b2ff91
sohitkumarmca2025-cmd <sohit.kumar.mca.2025@miet.ac.in>
2026-02-24 15:46:10 +0530

    Create 88-Count_Words_In_Sentence.py

---
+++ 100_program/88-Count_Words_In_Sentence.py
@@ -0,0 +1,12 @@
+# Count number of words in a sentence
+
+sentence = input("Enter sentence: ")
+
+words = sentence.split()
+
+print("Number of Words:", len(words))
+
+
+# Output:
+# Enter sentence: I love python programming
+# Number of Words: 4
