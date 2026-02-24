009f409a5b3c521c7c4c50a30ce43b9bafa9293b
sohitkumarmca2025-cmd <sohit.kumar.mca.2025@miet.ac.in>
2026-02-24 14:17:40 +0530

    Create Factorial_For_Loop.py

---
+++ 100_program/Factorial_For_Loop.py
@@ -0,0 +1,16 @@
+# Find factorial using for loop
+
+n = int(input("Enter number: "))
+
+if n < 0:
+    print("Number invalid!")
+else:
+    fact = 1
+    for i in range(1, n + 1):
+        fact *= i
+
+    print("Factorial:", fact)
+
+# Output:
+# Enter number: 5
+# Factorial: 120
