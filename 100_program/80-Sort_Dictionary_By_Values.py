fa15a8ed05d59f4473d48af1e38cf7554e09234e
sohitkumarmca2025-cmd <sohit.kumar.mca.2025@miet.ac.in>
2026-02-24 15:39:36 +0530

    Create 80-Sort_Dictionary_By_Values.py

---
+++ 100_program/80-Sort_Dictionary_By_Values.py
@@ -0,0 +1,17 @@
+# Sort dictionary by values
+
+data = eval(input("Enter dictionary: "))
+
+sorted_items = sorted(data.items(), key=lambda x: x[1])
+
+sorted_dict = {}
+
+for key, value in sorted_items:
+    sorted_dict[key] = value
+
+print("Sorted by Values:", sorted_dict)
+
+
+# Output:
+# Enter dictionary: {'a':3,'b':1,'c':2}
+# Sorted by Values: {'b': 1, 'c': 2, 'a': 3}
