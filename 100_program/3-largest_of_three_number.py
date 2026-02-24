02d6df838b4ff3a2add876e467095d3ec7889e47
sohitkumarmca2025-cmd <sohit.kumar.mca.2025@miet.ac.in>
2026-02-24 10:28:22 +0530

    Create 3-largest_of_three_number.py

---
+++ 100_program/3-largest_of_three_number.py
@@ -0,0 +1,15 @@
+# Find largest of three numbers
+
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+c = int(input("Enter third number: "))
+
+largest = max(a, b, c)
+
+print("Largest Number:", largest)
+
+# Output:
+# Enter first number: 10
+# Enter second number: 25
+# Enter third number: 15
+# Largest Number: 25
