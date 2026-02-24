9a188c658c0f0eb12d751ce0bb19e1da656c0ff8
sohitkumarmca2025-cmd <sohit.kumar.mca.2025@miet.ac.in>
2026-02-24 15:55:09 +0530

    Create 98-Check_String_Only_Digits.py

---
+++ 100_program/98-Check_String_Only_Digits.py
@@ -0,0 +1,13 @@
+# Check whether string contains only digits
+
+text = input("Enter string: ")
+
+if text.isdigit():
+    print("String contains only digits")
+else:
+    print("String does not contain only digits")
+
+
+# Output:
+# Enter string: 12345
+# String contains only digits
