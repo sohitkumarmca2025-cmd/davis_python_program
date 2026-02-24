81d1b4340db879c5d7945e50b52501d5b5a2b3c7
sohitkumarmca2025-cmd <sohit.kumar.mca.2025@miet.ac.in>
2026-02-24 15:25:51 +0530

    Create 60-Common_Elements_Two_Lists.py

---
+++ 100_program/60-Common_Elements_Two_Lists.py
@@ -0,0 +1,18 @@
+# Find common elements between two lists
+
+list1 = list(map(int, input("Enter first list: ").split()))
+list2 = list(map(int, input("Enter second list: ").split()))
+
+common = []
+
+for num in list1:
+    if num in list2 and num not in common:
+        common.append(num)
+
+print("Common Elements:", common)
+
+
+# Output:
+# Enter first list: 1 2 3 4
+# Enter second list: 3 4 5 6
+# Common Elements: [3, 4]
