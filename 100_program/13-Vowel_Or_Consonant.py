bce7f8193a3fd945f720f4ee67deeb3fed1f1f94
sohitkumarmca2025-cmd <sohit.kumar.mca.2025@miet.ac.in>
2026-02-24 11:26:45 +0530

    Create 13-Vowel_Or_Consonant.py

---
+++ 100_program/13-Vowel_Or_Consonant.py
@@ -0,0 +1,12 @@
+# Check vowel or consonant
+
+ch = input("Enter a character: ")
+
+if ch.lower() in ['a', 'e', 'i', 'o', 'u']:
+    print("Vowel")
+else:
+    print("Consonant")
+
+# Output:
+# Enter a character: e
+# Vowel      
