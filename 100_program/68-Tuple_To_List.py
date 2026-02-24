d8026b9e64649e723a8984d2b65d1c1faa59552b
sohitkumarmca2025-cmd <sohit.kumar.mca.2025@miet.ac.in>
2026-02-24 15:30:32 +0530

    Create 68-Tuple_To_List.py

---
+++ 100_program/68-Tuple_To_List.py
@@ -0,0 +1,12 @@
+# Convert tuple to list
+
+numbers = tuple(map(int, input("Enter numbers: ").split()))
+
+converted = list(numbers)
+
+print("Converted List:", converted)
+
+
+# Output:
+# Enter numbers: 1 2 3 4
+# Converted List: [1, 2, 3, 4]
