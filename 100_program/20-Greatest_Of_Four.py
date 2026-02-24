44dddcc0415cb537e4257c6e8d3664d178878722
sohitkumarmca2025-cmd <sohit.kumar.mca.2025@miet.ac.in>
2026-02-24 12:09:38 +0530

    Create 20-Greatest_Of_Four.py

---
+++ 100_program/20-Greatest_Of_Four.py
@@ -0,0 +1,17 @@
+# Find greatest of four numbers
+
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+c = int(input("Enter third number: "))
+d = int(input("Enter fourth number: "))
+
+greatest = max(a, b, c, d)
+
+print("Greatest Number:", greatest)
+
+# Output:
+# Enter first number: 10
+# Enter second number: 25
+# Enter third number: 15
+# Enter fourth number: 30
+# Greatest Number: 30
