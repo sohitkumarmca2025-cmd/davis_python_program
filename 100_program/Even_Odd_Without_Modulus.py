fed9b00f780811a37267f3e0d373079e04b2953b
sohitkumarmca2025-cmd <sohit.kumar.mca.2025@miet.ac.in>
2026-02-24 10:58:22 +0530

    Create Even_Odd_Without_Modulus.py

---
+++ 100_program/Even_Odd_Without_Modulus.py
@@ -0,0 +1,12 @@
+# Check even or odd without modulus
+
+num = int(input("Enter a number: "))
+
+if (num // 2) * 2 == num:
+    print("Even Number")
+else:
+    print("Odd Number")
+
+# Output:
+# Enter a number: 7
+# Odd Number
