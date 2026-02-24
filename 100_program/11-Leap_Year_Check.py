9ecd31f83a884007ac7c3c1950e7d386bfcec79d
sohitkumarmca2025-cmd <sohit.kumar.mca.2025@miet.ac.in>
2026-02-24 11:15:23 +0530

    Create 11-Leap_Year_Check.py

---
+++ 100_program/11-Leap_Year_Check.py
@@ -0,0 +1,12 @@
+# Check whether a year is leap year
+
+year = int(input("Enter year: "))
+
+if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+    print("Leap Year")
+else:
+    print("Not a Leap Year")
+
+# Output:
+# Enter year: 2024
+# Leap Year
