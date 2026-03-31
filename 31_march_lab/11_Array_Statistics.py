d5bda2045045a3fc8c39a86427bcb45b0394d826
sohitkumarmca2025-cmd <sohit.kumar.mca.2025@miet.ac.in>
2026-03-31 11:17:50 +0530

    Create 11_Array_Statistics.py

---
+++ 31_march_lab/11_Array_Statistics.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+arr = np.array([1, 2, 3, 4, 5])
+
+print("Mean:", np.mean(arr), "Median:", np.median(arr), "Std:", np.std(arr))
+
+# Output:
+# Mean: 3.0 Median: 3.0 Std: 1.4142135623730951
